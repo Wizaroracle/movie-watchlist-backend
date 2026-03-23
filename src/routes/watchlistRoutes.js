708a90330f6f07f61ffbc9359ed419441e5b4cb4
Christian Jonel Cañanes <christianjonelcananes@gmail.com>
2026-03-24 06:10:41 +0800

    implement authentication bearer validation and middleware for watchlist api

--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { addToWatchList } from "../controllers/watchlistController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
+
+router.use(authMiddleware);
 
 router.post("/", addToWatchList);
 
