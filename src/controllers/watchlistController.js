c6caab4a1d293ea7ed541e383a5c369959d9ce19
Christian Jonel Cañanes <christianjonelcananes@gmail.com>
2026-03-24 05:06:55 +0800

    finishing the watchlist controller

--- src/controllers/watchlistController.js
+++ src/controllers/watchlistController.js
@@ -1,7 +1,7 @@
-import { prisma } from "../config/db";
+import { prisma } from "../config/db.js";
 
 const addToWatchList = async (req, res) => {
-  const { movieId, status, rating, notes } = req.body;
+  const { movieId, status, rating, notes, userId } = req.body;
 
   //Veriy movie exists
   const movie = await prisma.movie.findUnique({
@@ -16,7 +16,31 @@ const addToWatchList = async (req, res) => {
 
   //Check if already added
   const existingInWatch = await prisma.watchlistItem.findUnique({
-    where: { id: movieId },
+    where: {
+      userId_movieId: {
+        userId: userId,
+        movieId: movieId,
+      },
+    },
+  });
+
+  if (existingInWatch) {
+    return res.status(400).json({ error: "Movie already in the watchlist" });
+  }
+
+  const watchlistItem = await prisma.watchlistItem.create({
+    data: {
+      userId,
+      movieId,
+      status: status || "PLANNED",
+      rating,
+      notes,
+    },
+  });
+
+  res.status(201).json({
+    status: "Success",
+    data: { watchlistItem },
   });
 };
 
